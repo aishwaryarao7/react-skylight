8974f65c4ec11546f6a736529d0cd130b3fd2468
Aishwarya Rao <aishwarya.rao@sjsu.edu>
2019-09-15 15:18:42 +0530

    Change dialogStyles to css

--- src/skylightstateless.jsx
+++ src/skylightstateless.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './styles';
 import assign from './utils/assign';
+import './styles.css';
 
 export default class SkyLightStateless extends React.Component {
 
